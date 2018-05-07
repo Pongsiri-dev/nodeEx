566c098dc427a2657471beddf2e917cf9b53a616
slumboy <Taa_wappey@windowslive.com>
2018-05-07 18:01:31 +0700

    New write code

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var Conn = require('http')
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
 
-Conn.createServer(function(req,res){
-    console.log("Server is running... ");
-}).listen(9999);
+const serv = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text-plain');
+    res.end('Hello Word of MeanStack (Node.js  + Javascript es6 )');
+});
+
+serv.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
