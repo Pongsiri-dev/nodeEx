5be4b9f8f19740df9c040ad1ceb10f57cd271da2
slumboy <Taa_wappey@windowslive.com>
2018-05-10 14:28:45 +0700

    Create Connection to DB2

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const http = require('http');
-const hostname = 'localhost';
-const port = 3000;
-
-const serv = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text-plain');
-    res.end('Hello Word of MeanStack (Node.js  + Javascript es6 )');
-});
-
-serv.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const fs = require('fs');
+let fd;
+const ibmdb = require('ibm_db'),
+connString="DATABASE=EGPDB;HOSTNAME=10.10.8.53;UID=egp3uat;PWD=uategp3;PORT=50099;PROTOCOL=TCPIP";
+try {
+    var option = { connectTimeout : 40, systemNaming : true };// Connection Timeout after 40 seconds.
+    var conn = ibmdb.openSync(connString, option);
+    conn.query("Select * from EGPPROCURE2.PROCURE_150_AUDIT_LOG", function (err, rows) {
+      if (err) {
+          console.log(err);
+      } else {
+        fs.writeFile('helloworld.txt', rows, function (err) {
+            if (err) 
+                return console.log(err);
+            console.log('Wrote Hello World in file helloworld.txt, just check it');
+        });
+        //console.log(rows);
+      }
+      conn.close();	
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
